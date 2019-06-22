728498704a25b78af8f648dacbff8e9e8856305f
deinek <51771517+deinek@users.noreply.github.com>
2019-06-22 07:20:07 +0300

    Add files via upload

---
+++ AnotepadObjects.java
@@ -0,0 +1,71 @@
+package objects;
+
+import io.qameta.allure.Step;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AnotepadObjects {
+
+    private static final By registerLogin = By.cssSelector("a[href*='account']");
+    private static final By forgotPassword = By.cssSelector("a[href*='forgot']");
+    private static final By fieldEmail = By.cssSelector("#email");
+    private static final By resetPassButton = By.cssSelector("#some_name");
+    private static final By confirmationMessage = By.cssSelector(".container");
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public AnotepadObjects (WebDriver driver)
+    {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver,5);
+    }
+
+    @Step
+    public AnotepadObjects openSite()
+    {
+        driver.get("https://anotepad.com/");
+        return this;
+    }
+
+    @Step
+    public AnotepadObjects login()
+    {
+        driver.findElement(registerLogin).click();
+        return this;
+    }
+
+    @Step
+    public AnotepadObjects clickForgotPass()
+    {
+        driver.findElement(forgotPassword).click();
+        return this;
+    }
+
+    @Step
+    public AnotepadObjects enterEmail()
+    {
+        driver.findElement(fieldEmail).sendKeys("deinek@ukr.net");
+        return this;
+    }
+
+    @Step
+    public AnotepadObjects clickResetButton()
+    {
+        driver.findElement(resetPassButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(confirmationMessage));
+        return this;
+    }
+
+    @Step
+    public AnotepadObjects verification()
+    {
+        Assert.assertTrue(true);
+        Assert.assertEquals("An email has been sent to you with instructions on resetting your password.",confirmationMessage);
+        return this;
+    }
+
+}
---
+++ HomeWork3.java
@@ -0,0 +1,38 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import objects.AnotepadObjects;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static objects.AnotepadObjects.*;
+
+public class HomeWork3 {
+
+    WebDriver driver;
+    AnotepadObjects np;
+
+    @Before
+    public void openBrowser()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        np = new AnotepadObjects(driver);
+    }
+
+    @Test
+    public void resetPassword() {
+        np.openSite();
+        np.login();
+        np.clickForgotPass();
+        np.enterEmail();
+        np.clickResetButton();
+    }
+
+    @After
+    public void closeBrowser()
+    {
+        driver.quit();
+    }
+}

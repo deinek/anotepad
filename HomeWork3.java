14886992710a84697092b31991d352149d9cfe44
deinek <51771517+deinek@users.noreply.github.com>
2019-06-27 22:34:25 +0300

    Update HomeWork3.java

--- HomeWork3.java
+++ HomeWork3.java
@@ -34,4 +34,40 @@ public class HomeWork3 {
     {
         driver.quit();
     }
+}import io.github.bonigarcia.wdm.WebDriverManager;
+import objects.AnotepadObjects;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeWork3 {
+
+    WebDriver driver;
+    AnotepadObjects np;
+
+    @Before
+    public void openBrowser()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        np = new AnotepadObjects(driver);
+    }
+
+    @Test
+    public void resetPassword() {
+        np.openSite();
+        np.login();
+        np.clickForgotPass();
+        np.enterEmail();
+        np.clickResetButton();
+        np.verification();
+    }
+
+    @After
+    public void closeBrowser()
+    {
+        driver.quit();
+    }
 }
